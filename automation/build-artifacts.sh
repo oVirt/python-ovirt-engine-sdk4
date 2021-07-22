336395ecb057c8861d38b8f797b264deeb5cdb6e
mnecas <necas.marty@gmail.com>
2021-07-22 11:00:34 +0200

    remove setup.py and use build.sh

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -1,4 +1,37 @@
-#!/usr/bin/bash -xe
-export PYTHON=python3
+#!/bin/bash -xe
 
-${PYTHON} automation/build.py
+ROOT_PATH=$PWD
+
+# remove any previous artifacts
+rm -f ./*tar.gz $ROOT_PATH/exported-artifacts
+
+# Create exported-artifacts
+[[ -d exported-artifacts ]] || mkdir -p $ROOT_PATH/exported-artifacts
+
+# Create build
+
+./build.sh build
+
+# create the src.rpm
+rpmbuild \
+    -D "_srcrpmdir $ROOT_PATH/output" \
+    -D "_topmdir $ROOT_PATH/rpmbuild" \
+    -ts ./*.gz
+
+# install any build requirements
+yum-builddep $ROOT_PATH/output/*src.rpm
+
+# create the rpms
+rpmbuild \
+    -D "_rpmdir $ROOT_PATH/output" \
+    -D "_topmdir $ROOT_PATH/rpmbuild" \
+    --rebuild  $ROOT_PATH/output/*.src.rpm
+
+# Store any relevant artifacts in exported-artifacts for the ci system to
+# archive
+find $ROOT_PATH/output -iname \*rpm -exec mv "{}" $ROOT_PATH/exported-artifacts/ \;
+
+# Export build to artifacts
+mv $ROOT_PATH/*tar.gz $ROOT_PATH/exported-artifacts/
+
+cd $ROOT_PATH
---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+VERSION="4.4.17"
+MILESTONE=master
+RPM_RELEASE="0.1.$MILESTONE.$(date -u +%Y%m%d%H%M%S)"
+
+PACKAGE_NAME="python-ovirt-engine-sdk4"
+
+RPM_VERSION=$VERSION
+PACKAGE_VERSION=$VERSION
+[ -n "$MILESTONE" ] && PACKAGE_VERSION+="_$MILESTONE"
+DISPLAY_VERSION=$PACKAGE$VERSION
+
+TARBALL="$PACKAGE_NAME-$PACKAGE_VERSION.tar.gz"
+
+
+dist() {
+  echo "Creating tar archive '$TARBALL' ... "
+  sed \
+   -e "s|@RPM_VERSION@|$RPM_VERSION|g" \
+   -e "s|@RPM_RELEASE@|$RPM_RELEASE|g" \
+   -e "s|@PACKAGE_NAME@|$PACKAGE_NAME|g" \
+   -e "s|@PACKAGE_VERSION@|$PACKAGE_VERSION|g" \
+   < python-ovirt-engine-sdk4.spec.in > python-ovirt-engine-sdk4.spec
+
+  find ./* -not -name '*.spec' -type f | tar --files-from /proc/self/fd/0 -czf "$TARBALL" python-ovirt-engine-sdk4.spec
+  echo "tar archive '$TARBALL' created."
+}
+
+build() {
+    dist
+}
+
+$1
