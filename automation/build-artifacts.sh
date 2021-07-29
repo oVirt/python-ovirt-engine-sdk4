bbbb0c7dffb501cccd3b89512cef0f3e042b3e8c
mnecas <necas.marty@gmail.com>
2021-07-29 09:11:02 +0200

    add nil debug_package

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -15,7 +15,6 @@ rm -f ./*tar.gz
 rpmbuild \
     -D "_srcrpmdir $PWD/output" \
     -D "_topmdir $PWD/rpmbuild" \
-    -D "_builddir $PWD/" \
     -ts ./*.gz
 
 # install any build requirements
@@ -25,7 +24,6 @@ yum-builddep output/*src.rpm
 rpmbuild \
     -D "_rpmdir $PWD/output" \
     -D "_topmdir $PWD/rpmbuild" \
-    -D "_builddir $PWD/" \
     --rebuild  output/*.src.rpm
 
 # Store any relevant artifacts in exported-artifacts for the ci system to
