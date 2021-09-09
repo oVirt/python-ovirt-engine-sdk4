68b9a4401efde47e586b9cc1b047c3da264ed8d6
Martin Nečas <necas.marty@gmail.com>
2021-09-09 15:44:30 +0200

    Fix el9stream automation (#9)
    * specified el9stream packages
    
    * remove extra line
    
    * remove python3-entrypoints

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -16,11 +16,15 @@ rpmbuild \
     -D "_srcrpmdir $PWD/output" \
     -D "_sourcedir $PWD/" \
     -D "_topmdir $PWD/rpmbuild" \
-    -bs ./*.spec
+    -bs ./python-ovirt-engine-sdk4.spec
 
-if [[ "$(rpm --eval "%dist")" == ".el9" ]]; then
-  # python38 is not available on el9!
-  rm -vf output/python38*
+if [[ "$(rpm --eval "%dist")" == ".el8" ]]; then
+# create the src.rpm
+rpmbuild \
+    -D "_srcrpmdir $PWD/output" \
+    -D "_sourcedir $PWD/" \
+    -D "_topmdir $PWD/rpmbuild" \
+    -bs ./python38-ovirt-engine-sdk4.spec
 fi
 
 # install any build requirements
