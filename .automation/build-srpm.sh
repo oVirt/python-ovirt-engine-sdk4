1ed93af784d696bf056342fe19997fe5a4673168
Martin Perina <mperina@redhat.com>
2022-11-10 13:57:27 +0100

    Fix issue around tarball generation
    Signed-off-by: Martin Perina <mperina@redhat.com>

--- .automation/build-srpm.sh
+++ .automation/build-srpm.sh
@@ -4,7 +4,8 @@ source $(dirname "$(readlink -f "$0")")/generate-setup-files.sh
 
 TARBALL="ovirt-engine-sdk-python-${PACKAGE_VERSION}.tar.gz"
 
-find . -not -name '*.spec' -not -name '*.in' -type f | tar --files-from /proc/self/fd/0 -czf "${TARBALL}" python-ovirt-engine-sdk4.spec
+find . -not -name '*.spec' -not -name '*.in' -not -name ${TARBALL} -type f | \
+    tar --files-from /proc/self/fd/0 -czf "${TARBALL}" python-ovirt-engine-sdk4.spec
 
 # Directory, where build artifacts will be stored, should be passed as the 1st parameter
 ARTIFACTS_DIR=${1:-exported-artifacts}
